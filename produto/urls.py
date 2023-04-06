8ee429cf80671dd0d67f4232627bff7f7d60c616
Matheus <matheus.eai@gmail.com>
2023-04-05 22:36:46 -0300

    Produto list

--- produto/urls.py
+++ produto/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('<slug>', views.DetalheProdutos.as_view(), name="detalhe"),
     path('adicionaraocarrinho/', views.AdicionarAoCarinho.as_view(),
          name="adicionaraoCarinho"),
-    path('removerdocarinhos/', views.RemoverDoCarinho.as_view(),
+    path('removerdocarinho/', views.RemoverDoCarinho.as_view(),
          name="removerdocarinhos"),
     path('carinho/', views.Carinho.as_view(), name="carinho"),
     path('finalizar/', views.Finalizar.as_view(), name="finalizar"),
